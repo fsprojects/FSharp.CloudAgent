7a9b04d6a6d6a9ab9d3a99962fb2c51858b37201
isaacabraham <isaac.abraham@gmail.com>
2014-10-11 23:10:25 +0100

    Tests against the Actor Factory.

--- src/FSharp.CloudAgent/Actors.fs
+++ src/FSharp.CloudAgent/Actors.fs
@@ -1,78 +1,83 @@
 ﻿namespace FSharp.CloudAgent.Actors
 
-open System.Collections.Generic
-open System
 open FSharp.CloudAgent
 open FSharp.CloudAgent.Messaging
+open System
+open System.Collections.Generic
 
 /// Manages lifetime of actors.
-type internal IActorStore<'a> =
-    /// Requests an actor for a particular key.
-    abstract member GetActor : ActorKey -> CloudAgentKind<'a>
-    /// Tells the store that an actor is no longer required and can be safely removed.
-    abstract member RemoveActor : ActorKey -> unit
+type internal IActorStore<'a> = 
     
-// Manages per-session actors
+    /// Requests an actor for a particular key.
+    abstract GetActor : ActorKey -> CloudAgentKind<'a>
+    
+    /// Tells the store that an actor is no longer required and can be safely removed.
+    abstract RemoveActor : ActorKey -> unit
+
+// Manages per-session actors.
 type private ActorStoreRequest =
-| Get of ActorKey
-| Remove of ActorKey
+    | Get of ActorKey
+    | Remove of ActorKey
 
-type private ActorFactory<'a> = MailboxProcessor<ActorStoreRequest * (AsyncReplyChannel<CloudAgentKind<'a>> option)>
+type private ActorStoreAgent<'a> = MailboxProcessor<ActorStoreRequest * AsyncReplyChannel<CloudAgentKind<'a>> option>
 
-module internal Factory =
-    let private createActorFactory<'a> createAgent =
-        let actorStore =
-            new ActorFactory<'a>
-                (fun inbox ->
-                    let actors = Dictionary<string, CloudAgentKind<'a>>()
-                    async {
-                        while true do
-                            let! message, replyChannel = inbox.Receive()
-                            match message with
-                            | Get (ActorKey agentId) ->
-                                let actor =
-                                    if not (actors.ContainsKey agentId) then
-                                        printfn "Creating actor %s" agentId
-                                        let actor =createAgent (ActorKey agentId)
-                                        actors.Add(agentId, actor)
-                                        match actor with
-                                        | ResilientCloudAgent actor -> try actor.Start() with _ -> ()
-                                        | BasicCloudAgent actor -> try actor.Start() with _ -> ()
-                                    actors.[agentId]
-                                replyChannel
-                                |> Option.iter(fun replyChannel -> replyChannel.Reply actor)
-                            | Remove (ActorKey actorKey) ->
-                                if actors.ContainsKey(actorKey) then
-                                    printfn "Removing actor %s" actorKey
-                                    let actorToRemove =
-                                        match actors.[actorKey] with
-                                        | ResilientCloudAgent actor -> actor :> IDisposable
-                                        | BasicCloudAgent actor -> actor :> IDisposable
-                                    actors.Remove actorKey |> ignore
-                                    actorToRemove.Dispose()
-                                        
-                    })
+module internal Factory = 
+    let private createActorStore<'a> createAgent = 
+        let actorStore = 
+            new ActorStoreAgent<'a>(fun inbox -> 
+                let actors = Dictionary<string, CloudAgentKind<'a>>()
+                async { 
+                    while true do
+                        let! message, replyChannel = inbox.Receive()
+                        match message with
+                        | Get(ActorKey agentId) -> 
+                            let actor = 
+                                if not (actors.ContainsKey agentId) then 
+                                    let actor = createAgent (ActorKey agentId)
+                                    actors.Add(agentId, actor)
+                                    match actor with
+                                    | ResilientCloudAgent actor -> 
+                                        try actor.Start()
+                                        with _ -> ()
+                                    | BasicCloudAgent actor -> 
+                                        try actor.Start()
+                                        with _ -> ()
+                                actors.[agentId]
+                            replyChannel |> Option.iter (fun replyChannel -> replyChannel.Reply actor)
+                        | Remove(ActorKey actorKey) -> 
+                            if actors.ContainsKey(actorKey) then 
+                                let actorToRemove = 
+                                    match actors.[actorKey] with
+                                    | ResilientCloudAgent actor -> actor :> IDisposable
+                                    | BasicCloudAgent actor -> actor :> IDisposable
+                                actors.Remove actorKey |> ignore
+                                actorToRemove.Dispose()
+                })
         actorStore.Start()
         actorStore
-        
+    
     /// Creates an IActorStore that can add / retrieve / remove agents in a threadsafe manner, using the supplied function to create new agents on demand.
-    let CreateActorStore<'a> createActor =
-        let actorFactory = createActorFactory<'a> createActor
+    let CreateActorStore<'a> createActor = 
+        let actorStore = createActorStore<'a> createActor
         { new IActorStore<'a> with
-            member __.GetActor(sessionId) = actorFactory.PostAndReply(fun ch -> (Get sessionId), Some ch)
-            member __.RemoveActor(sessionId) = actorFactory.Post(Remove sessionId, None) }
-        
+              member __.GetActor(sessionId) = actorStore.PostAndReply(fun ch -> (Get sessionId), Some ch)
+              member __.RemoveActor(sessionId) = actorStore.Post(Remove sessionId, None) }
+    
     /// Selects an agent to consume a message.
     type AgentSelectorFunc<'a> = unit -> Async<CloudAgentKind<'a>>
     
     /// Generates a pool of CloudAgents of a specific size that can be used to select an agent when required.
-    let CreateAgentSelector<'a> (size, createAgent : ActorKey -> CloudAgentKind<'a>) : AgentSelectorFunc<'a> = 
+    let CreateAgentSelector<'a>(size, createAgent : ActorKey -> CloudAgentKind<'a>) : AgentSelectorFunc<'a> = 
         let agents = 
-            [ for i in 1..size ->
-                let agent = createAgent (ActorKey (i.ToString()))
+            [ for i in 1 .. size -> 
+                let agent = createAgent (ActorKey <| i.ToString())
                 match agent with
-                | BasicCloudAgent agent -> try agent.Start() with _ -> ()
-                | ResilientCloudAgent agent -> try agent.Start() with _ -> ()
+                | BasicCloudAgent agent -> 
+                    try agent.Start()
+                    with _ -> ()
+                | ResilientCloudAgent agent -> 
+                    try agent.Start()
+                    with _ -> ()
                 agent ]
         
         let r = Random()
---
+++ tests/FSharp.CloudAgent.Tests/Actors.fs
@@ -0,0 +1,63 @@
+﻿[<NUnit.Framework.TestFixture>]
+module FSharp.CloudAgent.Tests.``Actor Factory``
+
+open FSharp.CloudAgent
+open FSharp.CloudAgent.Actors
+open FSharp.CloudAgent.Messaging
+open FSharp.CloudAgent.Tests.Helpers
+open NUnit.Framework
+open System
+open Swensen.Unquote
+
+let private getActorStore() = Factory.CreateActorStore(fun _ -> getBasicAgent() |> snd)
+
+let private assertAgentIsStarted (agent:MailboxProcessor<_>) =
+    Assertions.raisesWith<InvalidOperationException>
+        <@ agent.Start() @>
+        (fun ex -> <@ ex.Message = "The MailboxProcessor has already been started." @>)
+
+[<Test>]
+let ``ActorStore creates new actors``() =
+    let actorStore = getActorStore()
+    actorStore.GetActor (ActorKey "isaac") |> ignore
+
+[<Test>]
+let ``ActorStore Automatically starts agents``() =
+    let actorStore = getActorStore()
+    let (BasicCloudAgent isaac) = actorStore.GetActor (ActorKey "isaac")
+    isaac |> assertAgentIsStarted
+
+[<Test>]
+let ``ActorStore Manages multiple new actors``() =
+    let actorStore = getActorStore()
+    let isaac = actorStore.GetActor (ActorKey "isaac")
+    let tony = actorStore.GetActor (ActorKey "tony")
+    isaac <>? tony
+
+[<Test>]
+let ``ActorStore Reuses existing actors``() =
+    let actorStore = getActorStore()
+    let first = actorStore.GetActor (ActorKey "isaac")
+    let second = actorStore.GetActor (ActorKey "isaac")
+    first =? second
+
+[<Test>]
+let ``ActorStore Removes existing actors``() =
+    let actorStore = getActorStore()
+    let first = actorStore.GetActor (ActorKey "isaac")
+    actorStore.RemoveActor (ActorKey "isaac")
+    let second = actorStore.GetActor (ActorKey "isaac")
+    first <>? second
+
+[<Test>]
+let ``ActorStore Disposes of removed agents``() =
+    let actorStore = getActorStore()
+    let (BasicCloudAgent isaac) = actorStore.GetActor (ActorKey "isaac")
+    actorStore.RemoveActor (ActorKey "isaac")
+
+[<Test>]
+let ``AgentSelector creates already-started agents``() =
+    let selector = Factory.CreateAgentSelector(5, fun _ -> getBasicAgent() |> snd)
+    for _ in 1 .. 10 do
+        let (BasicCloudAgent agent) = selector() |> Async.RunSynchronously
+        agent |> assertAgentIsStarted
