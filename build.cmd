985084fb40c49ab4a5ed1391603ce0b97aeac7a9
Christian Steinert <chr.steinert@outlook.com>
2018-02-14 15:04:48 +0100

    Adds `dotnet restore` to build script

--- build.cmd
+++ build.cmd
@@ -6,6 +6,12 @@ IF NOT EXIST packages\FAKE\tools\FAKE.exe  (
     .paket\paket.exe restore
 )
 
+cd .\src\FSharp.CloudAgent\
+dotnet restore
+cd ..\..\tests\FSharp.CloudAgent.Tests\
+dotnet restore
+cd ..\..\
+
 IF NOT EXIST build.fsx (
   packages\FAKE\tools\FAKE.exe init.fsx
 )
